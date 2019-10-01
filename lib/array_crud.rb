d90e652179be53428c2ff0925822dc984a4b4774
wolfene2001 <wolfene2001@yahoo.com>
2019-10-01 00:20:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  stitches = ["knit", "purl", "yarn over", "slip stitch"]
+  stitches << "k2tog"
 end
 
 def add_element_to_start_of_array(array, element)
